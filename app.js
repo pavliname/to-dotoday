515a72d94a346a98ecf47094ca417fcf04e68927
Pavlina <pavlna@mail.usf.edu>
2020-04-22 10:46:06 -0400

    custom heroku config var implementation

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('trust proxy', true)
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect(process.env.CONNECTION, {useNewUrlParser:true});
+mongoose.connect(GITHUB_CONNECTION, {useNewUrlParser:true});
 
 const itemsSchema = {
   name: String
